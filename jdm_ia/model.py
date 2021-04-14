6cfae35f71331ef1cc9b5b10a8bbf988965d97c4
Johni Douglas Marangon <johni.douglas.marangon@gmail.com>
2021-04-14 16:37:07 -0300

    first commit

--- jdm_ia/model.py
+++ jdm_ia/model.py
@@ -1,6 +1,8 @@
 def predict():
+    import os
+
+    print('getcwd:      ', os.getcwd())
+    print('__file__:    ', __file__)
+    
     with open('./jdm_ia/models/generic.bin') as f:
         return f.read()
-
-# print(predict())
-        
