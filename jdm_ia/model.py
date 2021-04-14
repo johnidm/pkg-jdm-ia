f8a72d4b8175c261a129767d2feaf4320f53aa8a
Johni Douglas Marangon <johni.douglas.marangon@gmail.com>
2021-04-14 16:39:59 -0300

    first commit

--- jdm_ia/model.py
+++ jdm_ia/model.py
@@ -1,8 +1,11 @@
-def predict():
+def predict(model_name):
     import os
 
-    print('getcwd:      ', os.getcwd())
-    print('__file__:    ', __file__)
-    
-    with open('./jdm_ia/models/generic.bin') as f:
+    ROOT_DIR = os.path.abspath(os.getcwd())
+    model_path = os.path.join(ROOT_DIR, "models", model_name)
+
+    print('ROOT_DIR:      ', ROOT_DIR)
+    print('model_path:    ', model_path)
+
+    with open(model_path) as f:
         return f.read()
