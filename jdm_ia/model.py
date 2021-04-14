eaea04dbf901d539bcf20d657547d4dac67913e0
Johni Douglas Marangon <johni.douglas.marangon@gmail.com>
2021-04-14 17:03:48 -0300

    first commit

--- jdm_ia/model.py
+++ jdm_ia/model.py
@@ -1,13 +1,11 @@
 import os
 
 def predict(model_name):
-    ROOT_DIR = os.path.abspath(os.getcwd())
+    ROOT_DIR = os.path.dirname(os.path.realpath(__file__))
     model_path = os.path.join(ROOT_DIR, "models", model_name)
-    dir_path = os.path.dirname(os.path.realpath(__file__))
-
+    
     print('ROOT_DIR:      ', ROOT_DIR)
     print('model_path:    ', model_path)
-    print('dir_path:      ', dir_path)
 
     with open(model_path) as f:
         return f.read()
